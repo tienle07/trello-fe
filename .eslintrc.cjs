47443a6011c8c37ab1cf48f34a88e70cae9a13b5
Le Tien <tienle21032001@gmail.com>
2023-12-27 22:17:43 +0700

    Add Css Baseline

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -16,12 +16,23 @@ module.exports = {
     'react-refresh'
   ],
   rules: {
+
+    // react
     'react-refresh/only-export-components': 'warn',
     'react-hooks/rules-of-hooks': 'error',
     'react-hooks/exhaustive-deps': 'warn',
     'react/prop-types': 0,
     'react/display-name': 0,
 
+    // material
+    'no-restricted-imports': [
+      'error',
+      {
+        'patterns': ['@mui/*/*/*']
+      }
+    ],
+
+    // common
     'no-console': 1,
     'no-lonely-if': 1,
     'no-unused-vars': 1,
